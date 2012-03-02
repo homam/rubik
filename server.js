7102785e05c62a511cc7435dac7aea3bf9c90f97
Homam <bluesnowball@gmail.com>
2012-03-02 14:42:31 +0400

    Initial commit via nodester-cli

--- server.js
+++ server.js
@@ -1,43 +1,5 @@
-﻿/// <reference path="views/public/scripts/libs/_.js" />
-
-var express = require('express');
-if(typeof (_) == "undefined")
-    var _ = require('./views/public/scripts/libs/_.js');
-
-var port = process.env.PORT || parseInt(process.argv.pop());
-
-var app = express.createServer();
-
-app.configure(function () {
-    app.use(express.bodyParser());
-    app.use(app.router);
-    app.use('/public', express.static(__dirname + '/views/public'));
-});
-
-app.set('view engine', 'html');
-app.register('.html', require('jqtpl').express);
-
-app.get('/', function (req, res) {
-    res.render('index', {
-        layout: false,
-        release: "1" == req.query.release,
-        isMobile: (function () {
-            var mobileUAs = ['iphone', 'android', 'blackberry'];
-            var ua = req.headers['user-agent'];
-            if (!!ua) {
-                ua = ua.toLowerCase();
-                return _.any(mobileUAs, function (m) { return  ua.indexOf(m) > -1; });
-            }
-            return false;
-        })()
-    });
-});
-app.get('/homam', function (req, res) {
-    res.end('hhahah!');
-});
-
-
-// how to read a file: fs.readFile('socket.client.js', 'utf-8', function (a, d) { res.end(d); })
-
-app.listen(port);;
-
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\nApp (rubik) is running..');
+}).listen(15092);
